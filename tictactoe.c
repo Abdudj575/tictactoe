cb0807fbfd94c412dab34e7951fff73adea84951
abdu djuraev <abdudj575@gmail.com>
2025-08-08 12:52:38 -0500

    implemented user input + game logic

--- tictactoe.c
+++ tictactoe.c
@@ -40,9 +40,71 @@ bool checkWin(char board[3][3], char player){
     return 0;    
 }
 
+bool checkDraw(char board[3][3]){
+    for(int r = 0; r < 3; r++){
+        for(int c = 0; c < 3; c++){
+            if(board[r][c] == ' '){
+                return 0;
+            }
+        }
+    }
+
+    return 1;
+
+}
+
 int main()
 {
+    //welcome
+    printf("--Welcome to Tic-Tac-Toe!--\n");
+    printf("\n");
 
+    char player = 'X';
+
+    char board[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    int row, col;
+    printBoard(board);
+    
+    //start the game
+    
+    bool gameOver = 0;
+    bool turn = 0;
+    
+    
+    while(!gameOver){
+        
+        if(!turn){
+            player = 'X';
+        }
+        else{
+            player = 'O';
+        }
+
+        printf("%c's move: \n", player);
+        scanf("%d %d", &row, &col);
+        if(board[row][col] == ' ' && row <=2 && col <= 2){
+                board[row][col] = player;
+                printf("\n");
+                printBoard(board);
+                turn = !turn;
+        }
+
+        else{
+            printf("Invalid move!\n");
+            continue;
+        }
+
+        gameOver = checkWin(board, player) || checkDraw(board);    
+    
+    }
+
+    if(checkWin(board, player)){
+        printf("%c wins!\n", player);
+    } 
+    else{
+        printf("Draw!");
+    }   
+    
     return 0;
 
 }
